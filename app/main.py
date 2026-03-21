c93be554a69bfc2d2e17d46cc8396a3fa57ffb05
Sidhvi-1803 <sidhvi@efficientemenginerring.com>
2026-03-21 22:50:25 +0530

    Frontend connect with backend

--- app/main.py
+++ app/main.py
@@ -18,7 +18,14 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allow all origins (for development only)
+    allow_origins=["https://ai-career-assistant-fe.netlify.app",
+                   "http://localhost:5173",
+                   "http://127.0.0.1:5173",
+                   "https://ai-career-assistant-be-production.up.railway.app",
+                   "http://127.0.0.1:8000",
+                   "http://localhost:8000",
+    
+                   ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
